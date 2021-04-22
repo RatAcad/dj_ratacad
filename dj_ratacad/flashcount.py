b5f11891e78b884def59c844ae7a667db747c836
qdo1010 <do.q@husky.neu.edu>
2021-04-22 17:18:51 -0400

    add probe trials

--- dj_ratacad/flashcount.py
+++ dj_ratacad/flashcount.py
@@ -76,20 +76,29 @@ class FlashCountTrial(dj.Computed):
 
 		if not np.isnan(bpod_data["states"]["Correct"][0]):
 			dec_time = bpod_data["states"]["Correct"][0]
+			trial_data["isProbe"] = 0
+		
 		elif ("Error" in bpod_data["states"].keys()) and (
 			not np.isnan(bpod_data["states"]["Error"][0])
 		):
 			dec_time = bpod_data["states"]["Error"][0]
+			trial_data["isProbe"] = 0
+
 		elif ("ErrorProbe" in bpod_data["states"].keys()) and (
 			not np.isnan(bpod_data["states"]["ErrorProbe"][0])
 		):
 			dec_time = bpod_data["states"]["ErrorProbe"][0]
+			trial_data["isProbe"] = 1
+
 		elif ("CorrectProbe" in bpod_data["states"].keys()) and (
 			not np.isnan(bpod_data["states"]["CorrectProbe"][0])
 		):
 			dec_time = bpod_data["states"]["CorrectProbe"][0]
+			trial_data["isProbe"] = 1
+
 		else:
 			dec_time = None
+			trial_data["isProbe"] = 0
 
 		if ("Port1In" in bpod_data["events"]) and (
 			dec_time in np.atleast_1d(bpod_data["events"]["Port1In"])
