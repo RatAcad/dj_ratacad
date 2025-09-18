2edef4b34ed8952eafeec2ee7ca358cef9bebff6
Maxime Maheu <maheu.mp@gmail.com>
2025-09-18 10:22:29 -0400

    Option to send data from most recent protocol only

--- dj_ratacad_matlab/dj_matlab.m
+++ dj_ratacad_matlab/dj_matlab.m
@@ -10,17 +10,21 @@ ratacaddir = '/ad/eng/research/eng_research_scottlab/RATACAD_DATA';
 
 % Whether to drop existing tables
 data2push = 'all'; % 'all' or 'missing'
+protocolver = 'last'; % 'all' or 'last'
 
 % Run routine to update SQL database with behavioural data on network drive
 tic;
 try
-    dj_pushdatasets(ratacaddir, 'UncertainFlashInference', [], data2push);
-    dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v2', 'UncertainFlashInference', data2push);
-    dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v3', 'UncertainFlashInference', data2push);
-    dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v4', 'UncertainFlashInference', data2push);
-    dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v5', 'UncertainFlashInference', data2push);
-    dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v6', 'UncertainFlashInference', data2push);
-    dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v7', 'UncertainFlashInference', data2push);
+    if strcmpi(protocolver, 'all')        
+        dj_pushdatasets(ratacaddir, 'UncertainFlashInference'.  , []                       , data2push);
+        dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v2', 'UncertainFlashInference', data2push);
+        dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v3', 'UncertainFlashInference', data2push);
+        dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v4', 'UncertainFlashInference', data2push);
+        dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v5', 'UncertainFlashInference', data2push);
+        dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v6', 'UncertainFlashInference', data2push);
+    elseif strcmpi(protocolver, 'last')
+        dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v7', 'UncertainFlashInference', data2push);
+    end
 catch
     system(['/home/ratacad1/anaconda3/bin/curl -H "Tags: desktop_computer" ', ...
         '-d "Failed to publish new data on DataJoint" ntfy.sh/ratacademy']);
