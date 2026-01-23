f52253ef4c2eef020b02ac58e6e20f96ad227810
Maxime Maheu <maheu.mp@gmail.com>
2026-01-22 21:22:22 -0500

    Remove spaces

--- dj_ratacad_matlab/dj_matlab.m
+++ dj_ratacad_matlab/dj_matlab.m
@@ -1,6 +1,6 @@
 % Routine to update DataJoint SQL database with network data
 
-% Add toolboxes to the MATLAB path 
+% Add toolboxes to the MATLAB path
 addpath('/home/ratacad1/Documents/MATLAB/dj_ratacad/dj_ratacad_matlab');
 addpath('/home/ratacad1/Documents/MATLAB/datajoint-matlab');
 addpath('/home/ratacad1/MATLAB Add-Ons/Toolboxes/mym/distribution/mexa64');
@@ -15,7 +15,7 @@ protocolver = 'last'; % 'all' or 'last'
 % Run routine to update SQL database with behavioural data on network drive
 tic;
 try
-    if strcmpi(protocolver, 'all')        
+    if strcmpi(protocolver, 'all')
         dj_pushdatasets(ratacaddir, 'UncertainFlashInference'   , []                       , data2push);
         dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v2', 'UncertainFlashInference', data2push);
         dj_pushdatasets(ratacaddir, 'UncertainFlashInference_v3', 'UncertainFlashInference', data2push);
